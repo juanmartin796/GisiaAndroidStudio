2cdb057c742f204d28285241e89370fa74c32874
Juan Martin Fernandez <juanmartin_796@hotmail.com>
2018-10-05 00:33:00 -0300

    Se hizo el perceptron utilizando una operacion de NNAPI para hacer una red neuronal totalmente conectada

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -22,20 +22,29 @@ Java_gisia_martin_com_perceptron_MainActivity_model(JNIEnv *env, jobject instanc
     ANeuralNetworksModel* model = NULL;
     ANeuralNetworksModel_create(&model);
 
-    uint32_t dims[1] = {3};
+
     // In our example, all our tensors are matrices of dimension [3][4].
-    ANeuralNetworksOperandType entradas, pesos, sal, activacion;
+    ANeuralNetworksOperandType entradas, pesos, sal, activacion, bias;
     entradas.type= ANEURALNETWORKS_TENSOR_FLOAT32;
     entradas.scale = 0.f;
     entradas.zeroPoint = 0;
-    entradas.dimensionCount = 1;
-    entradas.dimensions = dims;
+    entradas.dimensionCount = 2;
+    uint32_t dimsInput[2] = {1,2};
+    entradas.dimensions = dimsInput;
 
     pesos.type= ANEURALNETWORKS_TENSOR_FLOAT32;
     pesos.scale = 0.f;
     pesos.zeroPoint = 0;
-    pesos.dimensionCount= 1;
-    pesos.dimensions = dims;
+    pesos.dimensionCount= 2;
+    uint32_t dimsPesos[2] = {1,2};
+    pesos.dimensions = dimsPesos;
+
+    bias.type= ANEURALNETWORKS_TENSOR_FLOAT32;
+    bias.scale = 0.f;
+    bias.zeroPoint = 0;
+    bias.dimensionCount= 1;
+    uint32_t dimsBias[1] = {1};
+    bias.dimensions = dimsBias;
 
     /*sal.type= ANEURALNETWORKS_FLOAT32;
     sal.scale = 0.f;
@@ -45,8 +54,9 @@ Java_gisia_martin_com_perceptron_MainActivity_model(JNIEnv *env, jobject instanc
     sal.type= ANEURALNETWORKS_TENSOR_FLOAT32;
     sal.scale = 0.f;
     sal.zeroPoint = 0;
-    sal.dimensionCount = 1;
-    sal.dimensions = dims;
+    sal.dimensionCount = 2;
+    uint32_t dimsSal[2] = {1,1};
+    sal.dimensions = dimsSal;
 
     activacion.type = ANEURALNETWORKS_INT32;
     activacion.scale = 0.f;
@@ -62,8 +72,9 @@ Java_gisia_martin_com_perceptron_MainActivity_model(JNIEnv *env, jobject instanc
     // Now we add the seven operands, in the same order defined in the diagram.
     ANeuralNetworksModel_addOperand(model, &entradas);  // operand 0
     ANeuralNetworksModel_addOperand(model, &pesos);  // operand 1
-    ANeuralNetworksModel_addOperand(model, &activacion);  // operand 2
-    ANeuralNetworksModel_addOperand(model, &sal); // operand 3
+    ANeuralNetworksModel_addOperand(model, &bias);  // operand 2
+    ANeuralNetworksModel_addOperand(model, &activacion);  // operand 3
+    ANeuralNetworksModel_addOperand(model, &sal); // operand 4
 
     /*uint32_t ent[3] = {1,1,1};
     const void *bufferEnt = ent;
@@ -71,24 +82,28 @@ Java_gisia_martin_com_perceptron_MainActivity_model(JNIEnv *env, jobject instanc
 
 
     //uint32_t pes[3] = {2,1,1};
-    float pes[3] = {-1.5,1.0,1.0};
+    float pes[2] = {1.0,1.0};
     const void *bufferPesos = pes;
     ANeuralNetworksModel_setOperandValue(model, 1, &pes, sizeof(pes));
 
+    float biasBuf[1] = {-1.5};
+    const void *bufferBias =biasBuf;
+    ANeuralNetworksModel_setOperandValue(model, 2, &biasBuf, sizeof(biasBuf));
+
     // We set the values of the activation operands, in our example operands 2 and 5.
     int32_t noneValue = ANEURALNETWORKS_FUSED_NONE;
-    ANeuralNetworksModel_setOperandValue(model, 2, &noneValue, sizeof(noneValue));
+    ANeuralNetworksModel_setOperandValue(model, 3, &noneValue, sizeof(noneValue));
 
 
     // We have two operations in our example.
     // The first consumes operands 1, 0, 2, and produces operand 4.
-    uint32_t addInputIndexes[3] = {0, 1,2};
-    uint32_t addOutputIndexes[1] = {3};
-    ANeuralNetworksModel_addOperation(model, ANEURALNETWORKS_MUL, 3, addInputIndexes, 1, addOutputIndexes);
+    uint32_t addInputIndexes[4] = {0, 1,2,3};
+    uint32_t addOutputIndexes[1] = {4};
+    ANeuralNetworksModel_addOperation(model, ANEURALNETWORKS_FULLY_CONNECTED, 4, addInputIndexes, 1, addOutputIndexes);
 
     // Our model has one input (0) and one output (6).
     uint32_t modelInputIndexes[1] = {0};
-    uint32_t modelOutputIndexes[1] = {3};
+    uint32_t modelOutputIndexes[1] = {4};
     ANeuralNetworksModel_identifyInputsAndOutputs(model, 1, modelInputIndexes, 1 ,modelOutputIndexes);
 
     ANeuralNetworksModel_finish(model);
@@ -108,7 +123,7 @@ Java_gisia_martin_com_perceptron_MainActivity_model(JNIEnv *env, jobject instanc
     ANeuralNetworksExecution* run1 = NULL;
     ANeuralNetworksExecution_create(compilation, &run1);
     // Set the single input to our sample model. Since it is small, we won’t use a memory buffer.
-    float myInput[3] = {1.0,1,1};
+    float myInput[2] = {1,1};
     ANeuralNetworksExecution_setInput(run1, 0, NULL, myInput, sizeof(myInput));
     // Set the output.
     float myOutput[1];
@@ -122,9 +137,10 @@ Java_gisia_martin_com_perceptron_MainActivity_model(JNIEnv *env, jobject instanc
     ANeuralNetworksExecution_free(run1);
 
 
-    float result = 0;
+    /*float result = 0;
     for (int i = 0; i < sizeof(myOutput); ++i) {
         result = result + myOutput[i];
     }
-    return result;
+    return result;*/
+    return myOutput[0];
 }
